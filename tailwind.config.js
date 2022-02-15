731a9d6ff86ac0799f73eaa3f49e5477d84187cd
yamanidev <elamine.yamani@gmail.com>
2022-02-15 12:53:10 +0100

    Added screens and paddings to them

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
         "./public/index.html"
     ],
     theme: {
+        screens: {
+            'sm': '640px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1280px',
+        },
         container: {
             center: true,
             padding: {
                 DEFAULT: "1rem",
+                "sm": "3rem",
+                "lg": "3.5rem"
             }
         },
         extend: {
